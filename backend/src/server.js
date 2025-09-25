6cf334746cc1a81a45e3af13853d989e80476155
Cassandra Ganska <89425242+CasGan@users.noreply.github.com>
2025-09-24 21:12:49 -0700

    signup section code refactoring

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,8 +25,18 @@ if(process.env.NODE_ENV === "production"){
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));  
     })
 }
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on Port: ${PORT}`);
+        });
+    })
+    .catch((err) =>{
+        console.error("Failed to connect to MongoDB: ", err);
+        process.exit(1);
+    });
 
-app.listen(PORT, () => {
-    console.log("Server running on port:" + PORT);
-    connectDB();
-}); 
+// app.listen(PORT, () => {
+//     console.log("Server running on port:" + PORT);
+//     connectDB();
+// }); 
