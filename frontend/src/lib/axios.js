36605ee718e0edd48a9be3630d4bdad4ded2080b
Cassandra Ganska <89425242+CasGan@users.noreply.github.com>
2025-09-27 20:54:16 -0700

    implements frontend setup

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,7 @@
+// axios to replace fetchapi 
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api", 
+    withCredentials: true, 
+});
