3b0d753eedd4fbd87ba90fa9dd478bc95c4177bc
Cassandra Ganska <89425242+CasGan@users.noreply.github.com>
2025-10-01 20:26:29 -0700

    implements real time messaging

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,42 +1,60 @@
-import { useChatStore } from '../store/useChatStore.js';
-import { useAuthStore } from '../store/useAuthStore.js';
-import ChatHeader from './ChatHeader.jsx';
-import { useEffect, useRef } from 'react';
-import NoChatHistoryPlaceholder from './NoChatHistoryPlaceholder.jsx';
-import MessageInput from './MessageInput.jsx';
-import MessagesLoadingSkeleton from './MessagesLoadingSkeleton.jsx';
+import { useEffect, useRef } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { useChatStore } from "../store/useChatStore";
+import ChatHeader from "./ChatHeader";
+import NoChatHistoryPlaceholder from "./NoChatHistoryPlaceholder";
+import MessageInput from "./MessageInput";
+import MessagesLoadingSkeleton from "./MessagesLoadingSkeleton";
 
 function ChatContainer() {
-  const {selectedUser, getMessagesByUserId, messages, isMessagesLoading} = useChatStore(); 
-  const {authUser} = useAuthStore();
+  const {
+    selectedUser,
+    getMessagesByUserId,
+    messages,
+    isMessagesLoading,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  } = useChatStore();
+  const { authUser } = useAuthStore();
   const messageEndRef = useRef(null);
 
-  useEffect(() =>{
+  useEffect(() => {
     getMessagesByUserId(selectedUser._id);
-  }, [selectedUser, getMessagesByUserId]);
+    subscribeToMessages();
+
+    // clean up
+    return () => unsubscribeFromMessages();
+  }, [selectedUser, getMessagesByUserId, subscribeToMessages, unsubscribeFromMessages]);
 
   useEffect(() => {
-    if(messageEndRef.current){
+    if (messageEndRef.current) {
       messageEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [messages]);
 
-
   return (
     <>
-      <ChatHeader /> 
-
-      <div className='flex-1 px-6 overflow-y-auto py-8'>
+      <ChatHeader />
+      <div className="flex-1 px-6 overflow-y-auto py-8">
         {messages.length > 0 && !isMessagesLoading ? (
-          <div className='max-w-3xl mx-auto space-y-6'> 
-            {messages.map(msg => (
-              <div key={msg._id} className={`chat ${msg.senderId === authUser._id ? "chat-end" : "chat-start"}`}>
-                <div className={`chat-bubble relative ${msg.senderId === authUser._id ? "bg-cyan-600 text-white" : "bg-slate-800 text-slate-200"}`}> 
+          <div className="max-w-3xl mx-auto space-y-6">
+            {messages.map((msg) => (
+              <div
+                key={msg._id}
+                className={`chat ${msg.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+              >
+                <div
+                  className={`chat-bubble relative ${
+                    msg.senderId === authUser._id
+                      ? "bg-cyan-600 text-white"
+                      : "bg-slate-800 text-slate-200"
+                  }`}
+                >
                   {msg.image && (
-                    <img src={msg.image} alt='Shared' className='rounded-lg h-48 object-cover'/>
+                    <img src={msg.image} alt="Shared" className="rounded-lg h-48 object-cover" />
                   )}
-                  {msg.text && <p className='mt-2'>{msg.text}</p>}
-                  <p className='text-xs mt-1 opacity-75 flex items-center gap-1'>
+                  {msg.text && <p className="mt-2">{msg.text}</p>}
+                  <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
                     {new Date(msg.createdAt).toLocaleTimeString(undefined, {
                       hour: "2-digit",
                       minute: "2-digit",
@@ -45,16 +63,19 @@ function ChatContainer() {
                 </div>
               </div>
             ))}
-            {/* message automatic scroller : scroll target */}
-            <div ref={messageEndRef}/>
+            {/* 👇 scroll target */}
+            <div ref={messageEndRef} />
           </div>
-        ) : isMessagesLoading ? <MessagesLoadingSkeleton /> : (
-          <NoChatHistoryPlaceholder name={selectedUser.fullName}/>
+        ) : isMessagesLoading ? (
+          <MessagesLoadingSkeleton />
+        ) : (
+          <NoChatHistoryPlaceholder name={selectedUser.fullName} />
         )}
       </div>
-      <MessageInput /> 
+
+      <MessageInput />
     </>
-  )
+  );
 }
 
-export default ChatContainer
+export default ChatContainer;
